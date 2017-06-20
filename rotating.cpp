b6872a003e8a9efc9d5448e6eaf568d850062b33
cavemanjaw <cavemanjaw@op.pl>
2017-06-20 21:18:01 +0200

    Inital commit, uploaded local offline work that was done

---
+++ rotating.cpp
@@ -0,0 +1,207 @@
+// reverse array
+//1. In form of table of pointers, allocate memory
+//2. In form of continuously allocated chunk of memory
+//3. Return a poniter to new array
+//4. Modify array
+//5. Array on stack?
+//6. Wyci¹gn¹æ z tegom bity
+//7. Rozbudowaæ interfejs o mo¿liwoœæ obrotu w dowoln¹ stronê
+
+//TODO There is a bug, most probebly in function responsible for rotating array allocated in one chunk
+//RESOLVED Printing was off
+
+#include <stdlib.h>
+#include <iostream>
+#include <time.h> 
+#include <string.h>
+
+//TODO
+
+//Add bit valued array type
+// 4 bits for each pixel
+//unsigned or signed, decide type for bitmap
+
+
+#define U16 short unsigned
+#define BITS_PER_BYTE
+
+//Bits per entry must fulfill 16 % bitsPerEntry = 0 
+//TODO: Add padding somehow
+//TODO: Investigate fulling, accesing and possibility of elastic handling
+
+//For division x/y it is q = (x + y - 1) / y;
+U16* AllocateBitArray(int bitsPerEntry, int matrixSize)
+{	
+	//take the celing of this to malloc
+	const int bitsAmount = bitsPerEntry * matrixSize * matrixSize;
+	U16* outputArray = (U16*)malloc((bitsAmount + BITS_PER_BYTE - 1) / BITS_PER_BYTE);
+}
+
+//allocate integral multiplicity of sizeof(U16)
+
+int* RandomCreateArrayOneChunk(int arraySize)
+{
+	//arrayDimension is the length of continuously allocated chunk of memory
+	//that is (arraySize * arraySize)
+	const int arrayDimension = arraySize * arraySize;
+	//Allocate memory for array in one chunk
+	int* outputArray = (int*)malloc(sizeof(int) * arrayDimension);
+	
+	for (int i = 0; i < arrayDimension; ++i)
+	{
+		outputArray[i] = rand();
+	}
+	
+	return outputArray;
+}
+
+int** RandomCreateArray(int arraySize)
+{
+	int** outputArray = (int**)malloc(sizeof(int*) * arraySize);
+	for (int i = 0; i < arraySize; ++i)
+	{
+		outputArray[i] = (int*)malloc(sizeof(int) * arraySize);
+	}
+
+	for (int i = 0; i < arraySize; ++i)
+	{
+		for (int j = 0; j < arraySize; ++j)
+		{
+			outputArray[i][j] = rand();
+		}
+	}
+
+	return outputArray;
+}
+
+void PrintArrayOneChunk(int* inputArray, int arraySize)
+{
+	for (int i = 0; i < arraySize; ++i)
+	{
+		for(int j = 0; j < arraySize; ++j)
+		{
+			std::cout << inputArray[(i * arraySize) + j] << " ";
+		}
+	std::cout << std::endl;
+	}
+}
+
+//For printing results
+void PrintArray(int** inputArray, int arraySize)
+{
+	for (int i = 0; i < arraySize; ++i)
+	{
+		for (int j = 0; j < arraySize; ++j)
+		{
+			std::cout << inputArray[i][j] << " ";
+		}
+	std::cout << std::endl;
+	}
+}
+
+//Returning array
+int** Rotate90Array(int** inputArray, int arraySize)
+{
+	int** outputArray = (int**)malloc(sizeof(int*) * arraySize);
+	
+	//Allocate memory for every pointer in outputArray[]
+	for (int i = 0; i < arraySize; ++i)
+	{
+		outputArray[i] = (int*)malloc(sizeof(int) * arraySize);
+	}
+
+	//Process newly allocated array
+	for (int i = 0; i < arraySize; ++i)
+	{
+		for (int j = 0; j < arraySize; ++j)
+		{
+			outputArray[i][j] = inputArray[j][arraySize - i - 1];
+		}
+	}
+
+	return outputArray;
+}
+
+//Can it be done as template? I mean, this would need 
+int* Rotate90ArrayOneChunk(int* inputArray, int arraySize)
+{
+	const int arrayDimension = arraySize * arraySize;
+	int* outputArray = (int*)malloc(sizeof(int) * arrayDimension);
+	
+	for (int i = 0; i < arraySize; ++i)
+	{
+		for (int j = 0; j < arraySize; ++j)
+		{
+			outputArray[(i * arraySize) + j] = inputArray[(j * arraySize) + (arraySize - i - 1)];
+		}
+	}
+
+	return outputArray;
+}
+
+//Modify array
+int** Rotate90ArrayModify(int** inputArray, int arraySize)
+{
+	//No new memory allocation needed
+	//Do it on stack
+	int originalArray[arraySize][arraySize];
+	
+	//Copy memory underneath inputArray's pointers
+	for (int i = 0; i < arraySize; ++i)
+	{
+		memcpy(&originalArray[i][0], inputArray[i], (sizeof(int) * arraySize));
+	}
+
+	for (int i = 0; i < arraySize; ++i)
+	{
+		for (int j = 0; j < arraySize; ++j)
+		{
+			inputArray[i][j] = originalArray[j][arraySize - i - 1];
+		}
+	}
+
+	return inputArray;
+}
+
+int main()
+{
+	//Seed the rand!
+	srand(time(NULL));
+
+	//For testing the output
+	const int arraySize = 7;
+	int** array = RandomCreateArray(arraySize);
+	int** rotatedArray = Rotate90Array(array, arraySize);
+
+	//Print original array
+	PrintArray(array, arraySize);
+	
+	std::cout << std::endl;
+
+	//Print rotated array
+	PrintArray(rotatedArray, arraySize);
+
+	std::cout << std::endl;
+	
+	//Modify and print original array
+	Rotate90ArrayModify(array, arraySize);
+	PrintArray(array, arraySize);	
+
+	std::cout << "*****************************" << std::endl;
+	std::cout << "One chunk array" << std::endl;
+	std::cout << std::endl;
+
+	//One chunk array
+	int* arrayOneChunk = RandomCreateArrayOneChunk(arraySize);
+	
+	PrintArrayOneChunk(arrayOneChunk, arraySize);
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	int* arrayOneChunkRotated = Rotate90ArrayOneChunk(arrayOneChunk, arraySize);
+
+	PrintArrayOneChunk(arrayOneChunkRotated, arraySize);
+
+	return 0; 
+}
